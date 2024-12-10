0e4d3a1e660df5d1a6d4e4667c36f0ed918058c1
manisha4450 <chowdaryc226@gmail.com>
2024-12-10 11:10:19 +0530

    Add files via upload

---
+++ nxt1st.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class nxt1st{
+    public static void main(String[] args){
+        String a[]={"apple,banana,grapes"};
+        for(String b:a)
+        System.out.println(b+" ");
+    } 
+}

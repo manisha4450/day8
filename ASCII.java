3796d682d638086af1f8845cd2c84af2fc646939
manisha4450 <chowdaryc226@gmail.com>
2024-12-10 14:12:08 +0530

    Add files via upload

---
+++ ASCII.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ASCII{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        char s=sc.next().charAt(0);
+        s^=32;
+        System.out.println(s);
+    }
+}
